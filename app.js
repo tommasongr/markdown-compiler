8139346f6d906575f1cff0d4f17998a54c0b97f6
Tommaso Negri <tommaso.negri98@gmail.com>
2018-03-19 23:34:58 +0100

    Setting Data & Bug Fix

--- app.js
+++ app.js
@@ -8,53 +8,99 @@ var content = fs.readFileSync('products.json');
 // Define to JSON type
 var jsonContent = JSON.parse(content);
 
+// Get current date using momentJs
 var currentDate = moment().format('YYYY-MM-DD');
 
 console.log('\nWriting Files...\n')
 
+// Set the if statement for Handlebars
+hbs.registerHelper('ifvalue', function(conditional, options) {
+    if(options.hash.value === conditional) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
+
 // Compiling Red Products
 
-for (let i = 0; i < jsonContent['products-red'].length; i++) {
-
-    var source = fs.readFileSync('tpl/productTpl.md', 'utf8');
-
-    var template = hbs.compile(source);
-
-    var data = { 'currentDate': currentDate, 'provaVar': jsonContent['products-red'][i].id };
-
-    var result = template(data);
-
-    fs.writeFile('results/red/' + jsonContent['products-red'][i].id + '.md', result, 'utf8', function(err){
-        if(err) {
-            return console.log(err);
+function checkDirectory(directory, callback) {
+    fs.stat(directory, function(err, stats){
+        if (err && err.errno === 34) {
+            fs.mkdir(directory, callback);
+        } else {
+            callback(err)
         }
     });
+}
 
-    console.log('Writing RED... ' + jsonContent['products-red'][i].id + '.md')
+checkDirectory("./results/", function(error) {  
+    if(error) {
+        console.log("Creating Directory Results...");
+        fs.mkdirSync('./results/');
+    } else {
+        //Carry on, all good, directory exists / created.
+    }
+});
 
-};
+checkDirectory("./results/red/", function(error) {  
+    if(error) {
+        console.log("Creating Directory Red...");
+        fs.mkdirSync('./results/red/');
+    } else {
+        //Carry on, all good, directory exists / created.
+    }
+});
 
-// Compiling Green Products
+checkDirectory("./results/green/", function(error) {  
+    if(error) {
+        console.log("Creating Directory Green...\n");
+        fs.mkdirSync('./results/green/');
+    } else {
+        //Carry on, all good, directory exists / created.
+    }
+});
 
-for (let i = 0; i < jsonContent['products-green'].length; i++) {
+setTimeout(() => {
+    for (let i = 0; i < jsonContent['products-red'].length; i++) {
 
-    var source = fs.readFileSync('tpl/productTpl.md', 'utf8');
-
-    var template = hbs.compile(source);
-
-    var data = { 'currentDate': currentDate, 'provaVar': jsonContent['products-green'][i].id };
-
-    var result = template(data);
-
-    fs.writeFile('results/green/' + jsonContent['products-green'][i].id + '.md', result, 'utf8', function(err){
-        if(err) {
-            return console.log(err);
-        }
-    });
-
-    console.log('Writing GREEN... ' + jsonContent['products-green'][i].id + '.md')
-
-};
-
-
-console.log('\nDONE\n');
+        var source = fs.readFileSync('tpl/productTpl.md', 'utf8');
+    
+        var template = hbs.compile(source);
+    
+        var data = { 'code': jsonContent['products-red'][i].code, 'name': jsonContent['products-red'][i].name, 'layout': jsonContent['products-red'][i].layout, 'currentDate': currentDate, 'img': jsonContent['products-red'][i].img, 'family': jsonContent['products-red'][i].family, 'line': jsonContent['products-red'][i].line, 'permalink': jsonContent['products-red'][i].permalink, 'energy': jsonContent['products-red'][i].energy, 'cadDraw': jsonContent['products-red'][i].cadDraw, 'exploded': jsonContent['products-red'][i].exploded, 'dataSheet': jsonContent['products-red'][i].dataSheet, 'dimensioni': jsonContent['products-red'][i].dimensioni, 'dimensioniInterne': jsonContent['products-red'][i].dimensioniInterne, 'griglieIncluse': jsonContent['products-red'][i].griglieIncluse, 'capacità': jsonContent['products-red'][i].capacità, 'pesoNetto': jsonContent['products-red'][i].pesoNetto, 'amperaggio': jsonContent['products-red'][i].amperaggio, 'consumo': jsonContent['products-red'][i].consumo, 'compressore': jsonContent['products-red'][i].compressore, 'modelloCompressore': jsonContent['products-red'][i].modelloCompressore, 'termostato': jsonContent['products-red'][i].termostato, 'modelloTermostato': jsonContent['products-red'][i].modelloTermostato, 'gasRefrigerante': jsonContent['products-red'][i].gasRefrigerante, 'temperatura': jsonContent['products-red'][i].temperatura, 'spessoreCoibentazione': jsonContent['products-red'][i].spessoreCoibentazione, 'alzatina': jsonContent['products-red'][i].alzatina};
+    
+        var result = template(data);
+    
+        fs.writeFile('results/red/' + jsonContent['products-red'][i].code + '.md', result, 'utf8', function(err){
+            if(err) {
+                return console.log(err);
+            }
+        });
+    
+        console.log('Writing RED... ' + jsonContent['products-red'][i].code + '.md')
+    
+    };
+    
+    // Compiling Green Products
+    
+    for (let i = 0; i < jsonContent['products-green'].length; i++) {
+    
+        var source = fs.readFileSync('tpl/productTpl.md', 'utf8');
+    
+        var template = hbs.compile(source);
+    
+        var data = { 'code': jsonContent['products-green'][i].code, 'name': jsonContent['products-green'][i].name, 'layout': jsonContent['products-green'][i].layout, 'currentDate': currentDate, 'img': jsonContent['products-green'][i].img, 'family': jsonContent['products-green'][i].family, 'line': jsonContent['products-green'][i].line, 'permalink': jsonContent['products-green'][i].permalink, 'energy': jsonContent['products-green'][i].energy, 'cadDraw': jsonContent['products-green'][i].cadDraw, 'exploded': jsonContent['products-green'][i].exploded, 'dataSheet': jsonContent['products-green'][i].dataSheet, 'dimensioni': jsonContent['products-green'][i].dimensioni, 'dimensioniInterne': jsonContent['products-green'][i].dimensioniInterne, 'griglieIncluse': jsonContent['products-green'][i].griglieIncluse, 'capacità': jsonContent['products-green'][i].capacità, 'pesoNetto': jsonContent['products-green'][i].pesoNetto, 'amperaggio': jsonContent['products-green'][i].amperaggio, 'consumo': jsonContent['products-green'][i].consumo, 'compressore': jsonContent['products-green'][i].compressore, 'modelloCompressore': jsonContent['products-green'][i].modelloCompressore, 'termostato': jsonContent['products-green'][i].termostato, 'modelloTermostato': jsonContent['products-green'][i].modelloTermostato, 'gasRefrigerante': jsonContent['products-green'][i].gasRefrigerante, 'temperatura': jsonContent['products-green'][i].temperatura, 'spessoreCoibentazione': jsonContent['products-green'][i].spessoreCoibentazione, 'alzatina': jsonContent['products-green'][i].alzatina};
+    
+        var result = template(data);
+    
+        fs.writeFile('results/green/' + jsonContent['products-green'][i].code + '.md', result, 'utf8', function(err){
+            if(err) {
+                return console.log(err);
+            }
+        });
+    
+        console.log('Writing GREEN... ' + jsonContent['products-green'][i].code + '.md')
+    
+    };
+}, 100);
--- tpl/productTpl.md
+++ tpl/productTpl.md
@@ -1,49 +1,40 @@
 ---
 code: {{code}}
 name: {{name}}
-layout: product
+layout: {{layout}}
 date: {{currentDate}}
 published: true
-img: /assets/img/products/EPF3422.jpg
-family: Armadi Refrigerati TN
+img: /assets/img/products/thumbs/{{img}}.jpg
+family: {{family}}
 offers: false
-permalink: /products/red/armadi-refrigerati-tn/:title/
+permalink: /products/{{line}}/{{permalink}}/:title/
 
 [//]: # (ADDITIONAL)
 
-energy: '{{#if energy}}  {{/if}}'
-cadDraw: '/assets/img/products/cad/EPF3421.pdf'
-exploded: ''
-dataSheet: ''
+energy: '{{#ifvalue energy value='SI'}}/assets/img/products/energy/{{line}}/EEI--{{code}}.png{{/ifvalue}}'
+cadDraw: '/assets/img/products/cad/{{code}}.pdf'
+exploded: '{{#ifvalue exploded value='SI'}}/assets/img/products/exploded/{{code}}.pdf{{/ifvalue}}'
+dataSheet: '{{#ifvalue dataSheet value='SI'}}/assets/img/products/data/{{code}}.pdf{{/ifvalue}}'
 ---
 
 
-| Dimensioni (WxDxH mm) | 1360×600×850 |
-| Dimensioni Interne (mm) | 822×570×580 |
-| Dimensioni Imballo (mm) | 1415×650×885 |
-| Griglie Incluse | 2 |
-| Dimensioni Griglie (mm) | 330×430 |
-| Peso Massimo Sostenibile per Griglia | 0 |
-| Capacità (l) | 220 |
-| Peso Netto (kg) | 90 |
-| Peso Lordo (kg) | 106 |
-| Voltaggio | 220-240/50/1 |
-| Potenza | 330 |
-| Amperaggio | 2,8 |
-| Consumo (kW/24h) | 2 |
-| Compressore | Embraco |
-| Modello Compressore | EMT65HLR |
-| Potenza Compressore (hp) | 1/4 |
-| Termostato | Carel |
-| Modello Termostato | PZYDS0PBE1 |
-| Potenza Sbrinamento (W) | 0 |
-| Tipo Refrigerante | R-134a |
-| Quantità Refrigerante Contenuta (g) | 330 |
-| Raffreddamento | Ventilato |
-| Temperatura (°C) | -2~8 |
-| Spessore Coibentazione (cm) | 6 |
-| Materiale Coibentante | C5H10 |
+| Dimensioni (LxPxA mm) | {{dimensioni}} |
+| Dimensioni Interne (mm) | {{dimensioniInterne}} |
+| Griglie Incluse | {{griglieIncluse}} |
+| Capacità (L) | {{capacità}} |
+| Peso Netto (Kg) | {{pesoNetto}} |
+| Amperaggio (A) | {{amperaggio}} |
+| Consumo (kW/24h) | {{consumo}} |
+| Compressore | {{compressore}} |
+| Modello Compressore | {{modelloCompressore}} |
+| Termostato | {{termostato}} |
+| Modello Termostato | {{modelloTermostato}} |
+| Gas Refrigerante | {{gasRefrigerante}} |
+| Temperatura (°C) | {{temperatura}} |
+| Spessore Coibentazione (cm) | {{spessoreCoibentazione}} |
 {: .info-specs-table}
 
+{{#ifvalue alzatina value='SI'}}
 "Disponibile in versione con Alzatina: EPF3421A"
 {: .extra-notes}
+{{/ifvalue}}
